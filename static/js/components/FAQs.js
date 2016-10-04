acfa2664f38499addebe1fa8cfc99e38d7538141
gaudi <craig@resin.io>
2016-10-04 15:50:47 +0100

    faqs

---
+++ static/js/components/FAQs.js
@@ -0,0 +1,12 @@
+const $ = require('jquery');
+const _ = require('lodash');
+const container = $('.FAQs-js')
+const html = container.data('html');
+var delimitedHtml = html.replace(/<h2>/gi, "<$delimiter><h2>");
+$(delimitedHtml.split('<$delimiter>')).each(function(index){
+  var q = $('<div class="FAQ">'+ this +'</div>')
+  header = q.find('h2')
+  header.wrap('<a class="FAQ__header" data-toggle="collapse" href="#faq-' + index + '"/>')
+  q.children('*').not('.FAQ__header').wrapAll('<div id="faq-' + index +  '" class="FAQ__content panel-collapse collapse" aria-expanded="true"/>');
+  container.append(q)
+})
